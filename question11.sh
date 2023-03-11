7225ec7e0d8b943c40c8b6abf0ac39a8b8842def
a1873270 <ananta.karmakar@student.adelaide.edu.au>
2023-03-11 11:55:25 +0000

    Stashing modified version of question11.sh

--- question11.sh
+++ question11.sh
@@ -3,7 +3,6 @@ git checkout -b branch2
 touch file4
 git add file4
 git commit -m "Add file4"
-nano file4
-git checkout -
+echo "Modified content" > file4
 git stash
-git stash apply
+git checkout main
