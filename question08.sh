fc5fd35214c8e71f669789851c62ff0ed6252db1
a1873270 <ananta.karmakar@student.adelaide.edu.au>
2023-03-11 09:57:06 +0000

    Move files by extension modified version of questio08.sh

--- question08.sh
+++ question08.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-# Create a directory named dir2
 mkdir dir2
-
-# Move all text files to dir2
 mv *.txt dir2/
-
-# Stage the changes
-git add dir2/*
-
-# Commit the changes
-git commit -m "Move all text files to dir2 directory"
+git add dir2/
+git commit -m "Moved all text files to dir2"
